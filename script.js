97e454759fde05159f7402e6f0d406c88aef82b6
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-11-19 21:23:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
+// Initialize Firebase with your configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC1kDtYIauw1KYF2OYK6zJBj_DtDxjuOWo",
+    authDomain: "login-signup-authenticat-4fa75.firebaseapp.com",
+    databaseURL: "https://login-signup-authenticat-4fa75-default-rtdb.firebaseio.com",
+    projectId: "login-signup-authenticat-4fa75",
+    storageBucket: "login-signup-authenticat-4fa75.appspot.com",
+    messagingSenderId: "77434343036",
+    appId: "1:77434343036:web:de2288d9664bae1c7483f9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const postForm = document.getElementById('postForm');
 const postList = document.getElementById('postList');
-const createPostForm = document.getElementById('createPostForm');
 
-function showCreatePostForm() {
-    createPostForm.style.display = 'block';
+function createPost() {
+    const postTitle = document.getElementById('postTitle').value;
+    const postContent = document.getElementById('postContent').value;
+
+    // Push the post data to Firebase
+    database.ref('posts').push({
+        title: postTitle,
+        content: postContent
+    });
+
+    // Clear the form fields
+    postForm.reset();
 }
 
-function addPost() {
-    const title = document.getElementById('postTitle').value;
-    const content = document.getElementById('postContent').value;
+// Listen for changes in the database and update the post list
+database.ref('posts').on('value', (snapshot) => {
+    postList.innerHTML = ''; // Clear previous posts
 
-    // Validate and add the post to the postList div
-    if (title && content) {
+    snapshot.forEach((childSnapshot) => {
+        const post = childSnapshot.val();
         const postItem = document.createElement('div');
-        postItem.innerHTML = `<h3>${title}</h3><p>${content}</p>`;
+        postItem.innerHTML = `<h3>${post.title}</h3><p>${post.content}</p>`;
         postList.appendChild(postItem);
-
-        // Clear the form
-        document.getElementById('postTitle').value = '';
-        document.getElementById('postContent').value = '';
-
-        // Hide the form after submission
-        createPostForm.style.display = 'none';
-    }
-}
-
-function deletePost() {
-    // Implement post deletion logic as needed
-    // You can use postList.removeChild() or other methods
-}
+    });
+});
