9453ebc7e5894d0591015c1af68f98aa6ae25ab3
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-11-19 23:05:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
-// Initialize Firebase with your configuration
+// Firebase configuration
 const firebaseConfig = {
+    // Your Firebase config details
     apiKey: "AIzaSyC1kDtYIauw1KYF2OYK6zJBj_DtDxjuOWo",
         authDomain: "login-signup-authenticat-4fa75.firebaseapp.com",
         databaseURL: "https://login-signup-authenticat-4fa75-default-rtdb.firebaseio.com",
             projectId: "login-signup-authenticat-4fa75",
         storageBucket: "login-signup-authenticat-4fa75.appspot.com",
         messagingSenderId: "77434343036",
-        appId: "1:77434343036:web:de2288d9664bae1c7483f9",
+        appId: "1:77434343036:web:de2288d9664bae1c7483f9"
 };
 
 firebase.initializeApp(firebaseConfig);
-
 const database = firebase.database();
-const postForm = document.getElementById('postForm');
-const postList = document.getElementById('postList');
 
+// Function to create a new post
 function createPost() {
     const postTitle = document.getElementById('postTitle').value;
     const postContent = document.getElementById('postContent').value;
 
-    // Push the post data to Firebase
-    database.ref('posts').push({
-        title: postTitle,
-        content: postContent
-    });
+    if (postTitle && postContent) {
+        const newPostRef = database.ref('posts').push();
+        newPostRef.set({
+            title: postTitle,
+            content: postContent
+        });
 
-    // Clear the form fields
-    postForm.reset();
+        // Clear the form after creating a post
+        document.getElementById('postTitle').value = '';
+        document.getElementById('postContent').value = '';
+    }
 }
 
-// Listen for changes in the database and update the post list
-database.ref('posts').on('value', (snapshot) => {
-    postList.innerHTML = ''; // Clear previous posts
+// Function to delete a post
+function deletePost(postId) {
+    const postRef = database.ref('posts/' + postId);
+    postRef.remove();
+}
 
-    snapshot.forEach((childSnapshot) => {
-        const post = childSnapshot.val();
-        const postItem = document.createElement('div');
-        postItem.innerHTML = `<h3>${post.title}</h3><p>${post.content}</p>`;
-        postList.appendChild(postItem);
+// Function to display posts
+function displayPosts() {
+    const postListDiv = document.getElementById('postList');
+
+    // Clear previous posts
+    postListDiv.innerHTML = '';
+
+    // Fetch posts from the database
+    database.ref('posts').on('value', (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            const post = childSnapshot.val();
+            const postId = childSnapshot.key;
+
+            // Create post HTML element
+            const postElement = document.createElement('div');
+            postElement.innerHTML = `
+                <h3>${post.title}</h3>
+                <p>${post.content}</p>
+                <button onclick="deletePost('${postId}')">Delete Post</button>
+            `;
+
+            // Append post to the postListDiv
+            postListDiv.appendChild(postElement);
+        });
     });
-});
+}
+
+// Display posts on page load
+displayPosts();
