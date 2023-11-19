8d238e0b467e963235fcd205704f940d368a3da2
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-11-20 00:50:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -85,6 +85,76 @@ database.ref('posts').on('value', (snapshot) => {
     });
 });
 
+// Check user authentication status
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in, check if the user is an admin
+        checkAdminStatus(user.uid);
+    } else {
+        // User is signed out, hide admin functionality
+        hideAdminFunctionality();
+    }
+});
+
+// Function to check if the user is an admin
+function checkAdminStatus(userId) {
+    // Replace 'YOUR_ADMIN_UID' with the actual UID of the admin user
+    const adminUid = 'YOUR_ADMIN_UID';
+
+    if (userId === adminUid) {
+        // User is an admin, show admin functionality
+        showAdminFunctionality();
+    } else {
+        // User is not an admin, hide admin functionality
+        hideAdminFunctionality();
+    }
+}
+
+// Function to show admin functionality
+function showAdminFunctionality() {
+    // Listen for changes in the database and update the post list with admin buttons
+    database.ref('posts').on('value', (snapshot) => {
+        postList.innerHTML = ''; // Clear previous posts
+
+        snapshot.forEach((childSnapshot) => {
+            const post = childSnapshot.val();
+            const postItem = document.createElement('div');
+
+            // Display post details with "Edit" and "Delete" buttons for admin
+            postItem.innerHTML = `
+                <h3>${post.title}</h3>
+                <p>${post.content}</p>
+                ${post.imageUrl ? `<img src="${post.imageUrl}" alt="Post Image">` : ''}
+                <button onclick="editPost('${childSnapshot.key}')">Edit</button>
+                <button onclick="deletePost('${childSnapshot.key}')">Delete</button>
+            `;
+
+            postList.appendChild(postItem);
+        });
+    });
+}
+
+// Function to hide admin functionality
+function hideAdminFunctionality() {
+    // Listen for changes in the database and update the post list without admin buttons
+    database.ref('posts').on('value', (snapshot) => {
+        postList.innerHTML = ''; // Clear previous posts
+
+        snapshot.forEach((childSnapshot) => {
+            const post = childSnapshot.val();
+            const postItem = document.createElement('div');
+
+            // Display post details without "Edit" and "Delete" buttons
+            postItem.innerHTML = `
+                <h3>${post.title}</h3>
+                <p>${post.content}</p>
+                ${post.imageUrl ? `<img src="${post.imageUrl}" alt="Post Image">` : ''}
+            `;
+
+            postList.appendChild(postItem);
+        });
+    });
+}
 
 function editPost(key) {
     selectedPostKey = key;
