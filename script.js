f67571ba4fe0f97f48a1dee420fc574a7381d73e
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-11-19 23:17:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Initialize Firebase with your configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyC1kDtYIauw1KYF2OYK6zJBj_DtDxjuOWo",
+        authDomain: "login-signup-authenticat-4fa75.firebaseapp.com",
+        databaseURL: "https://login-signup-authenticat-4fa75-default-rtdb.firebaseio.com",
+            projectId: "login-signup-authenticat-4fa75",
+        storageBucket: "login-signup-authenticat-4fa75.appspot.com",
+        messagingSenderId: "77434343036",
+        appId: "1:77434343036:web:de2288d9664bae1c7483f9"
 };
 
 firebase.initializeApp(firebaseConfig);
